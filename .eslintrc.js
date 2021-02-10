bb9583cfaafc2ffb7ebd880ddb17f638172cae72
Matthieu Haméon <matthieu.hameon@govirtuo.com>
2021-02-10 01:09:46 +0100

    chore, tune eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: { jsx: true },
+    ecmaFeatures: {jsx: true},
     ecmaVersion: 2018,
     sourceType: 'module'
   },
@@ -26,23 +26,23 @@ module.exports = {
     'plugin:import/recommended'
   ],
   rules: {
-    'array-bracket-spacing': [ 'error', 'always', {
+    'array-bracket-spacing': ['error', 'always', {
       arraysInArrays: false,
       objectsInArrays: false,
       singleValue: true
-    } ],
-    'eol-last': [ 'error', 'always' ],
-    'key-spacing': [ 'error', { 'afterColon': true, 'beforeColon': false, 'mode': 'strict' } ],
-    'keyword-spacing': [ 'error', { 'after': true, 'before': true } ],
+    }],
+    'eol-last': ['error', 'always'],
+    'key-spacing': ['error', {'afterColon': true, 'beforeColon': false, 'mode': 'strict'}],
+    'keyword-spacing': ['error', {'after': true, 'before': true}],
     'no-eval': 'error',
     'no-irregular-whitespace': 'error',
     'no-mixed-spaces-and-tabs': 'error',
     'no-multi-spaces': 'error',
     'no-multiple-empty-lines': 'error',
     'no-trailing-spaces': 'error',
-    'object-curly-spacing': [ 'error', 'always' ],
+    'object-curly-spacing': ['error', 'always'],
   },
-  overrides: [ {
+  overrides: [{
     extends: [
       'plugin:import/errors',
       'plugin:import/warnings',
@@ -53,24 +53,25 @@ module.exports = {
       'typescript-sort-keys',
       'import'
     ],
-    files: [ '**.ts', '**.tsx' ],
+    files: ['**.ts', '**.tsx'],
     rules: {
-      '@typescript-eslint/member-delimiter-style': [ 'error',
+      '@typescript-eslint/member-delimiter-style': ['error',
         {
-          'multiline': { 'delimiter': 'none', 'requireLast': false },
-          'singleline': { 'delimiter': 'semi', 'requireLast': false }
+          'multiline': {'delimiter': 'none', 'requireLast': false},
+          'singleline': {'delimiter': 'semi', 'requireLast': false}
         }
       ],
-      '@typescript-eslint/no-unused-vars': [ 'warn', {
+      '@typescript-eslint/no-unused-vars': ['warn', {
         args: 'none',
         ignoreRestSiblings: true,
         vars: 'all'
-      } ],
-      '@typescript-eslint/quotes': [ 'error', 'single' ],
-      '@typescript-eslint/semi': [ 'error', 'never' ],
-      '@typescript-eslint/explicit-function-return-type': [ 'error', { allowExpressions: true } ],
-      '@typescript-eslint/indent': [ 'error', 2 ],
-      'import/no-unresolved': [ 'off', { caseSensitive: false } ],
+      }],
+      '@typescript-eslint/quotes': ['error', 'single'],
+      '@typescript-eslint/semi': ['error', 'never'],
+      '@typescript-eslint/explicit-function-return-type': ['error', {allowExpressions: true}],
+      '@typescript-eslint/indent': ['error', 2],
+      "@typescript-eslint/explicit-module-boundary-types": ['error', {allowArgumentsExplicitlyTypedAsAny: true}],
+      'import/no-unresolved': ['off', {caseSensitive: false}],
     }
-  } ],
+  }],
 }
