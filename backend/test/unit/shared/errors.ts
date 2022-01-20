de6f731b8910d6d080855cff410f0ef04875e8bb
Matthieu Haméon <1923261+mhameon@users.noreply.github.com>
2022-01-20 16:28:25 +0100

    add tests for errors

---
+++ backend/test/unit/shared/errors.ts
@@ -0,0 +1,23 @@
+import { expect, UnexpectedError, InvalidArgumentError, NotFoundError } from '@shared/Errors'
+import assert from 'assert'
+
+describe('Errors', function () {
+  describe('expect', function () {
+    it('should throw UnexpectedError', function() {
+      try {
+        expect(new Error('Not an InvalidArgumentError'), InvalidArgumentError)
+        assert.fail('Should have thrown UnexpectedError')
+      } catch (e) {
+        assert.ok(e instanceof UnexpectedError)
+      }
+    })
+    it('should not throw UnexpectedError', function() {
+      try {
+        expect(new NotFoundError('not not found'), [ InvalidArgumentError, NotFoundError ])
+        assert.ok(true)
+      } catch (e) {
+        assert.fail('Should have not thrown UnexpectedError')
+      }
+    })
+  })
+})
